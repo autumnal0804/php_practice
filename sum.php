479dc90a1ef141bb734d377c0f9ee42453917a84
kou.yuiyui@gmail.com <ec2-user@ip-172-31-20-177.us-east-2.compute.internal>
2021-08-13 09:21:44 +0000

    sum.php

--- sum.php
+++ sum.php
@@ -1,4 +1,17 @@
 <?php
+function sum($i){
+        $result = $i*2;
+        return $result;
+}
+echo sum(8);
+
+function add($a, $b){
+    $resurt = $a + $b;{
+        return $resurt;
+    }
+}
+echo add(6, 7);
+
 $arr = array(1,3,5,7,9,11);
 
 echo array_product($arr);
